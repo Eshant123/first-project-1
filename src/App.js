dca66ed038864f864b8c1ca47886d5689f31ff04
eshant123 <yadavaeshant123@gmail.com>
2022-11-07 23:06:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='heading'>
+      <h2>Hello World!!</h2>
+      <h3>Hello World!!</h3>
+      <h3>Hello World!!</h3>
+      <h2>Hello World!!</h2>
+      <h2>Hello World!!</h2>
+    </div>
+  );
+}
+
+export default App;
